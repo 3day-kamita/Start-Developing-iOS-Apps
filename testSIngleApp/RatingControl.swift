a44ab5ca8f24be3dc4ed98484200d2c67b61f2cc
3day-kamita <kamita@3day.pet>
2019-09-17 09:25:56 +0900

    #1

--- testSIngleApp/RatingControl.swift
+++ testSIngleApp/RatingControl.swift
@@ -40,7 +40,6 @@ import UIKit
     //MARK: Private Methods
     
     private func setupButtons() {
-        
         for button in ratingButtons {
             removeArrangedSubview(button)
             button.removeFromSuperview()
@@ -65,6 +64,9 @@ import UIKit
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
+            // Set the accessibility label
+      //      button.accessibilityLabel = "Set \(Int(index) + 1) star rating"
+            
             button.addTarget(self, action:
                 #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
             
@@ -73,12 +75,65 @@ import UIKit
             ratingButtons.append(button)
         }
         
+        updateButtonSelectionStates()
+    
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            
+            button.isSelected = index < rating
+            
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 star set."
+            default:
+                valueString = "\(rating) stars set."
+            }
+            
+            // Assign the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
+            var rating = 0 {
+                didSet {
+                    updateButtonSelectionStates()
+                }
+            }
+        }
     }
     
     //MARK: Button Action
     
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
+    
+    
     
 }
